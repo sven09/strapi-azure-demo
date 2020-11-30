3696e12491bb14254e06453fcaecbe7237dca25b
Sven <37796423+sven09@users.noreply.github.com>
2020-11-30 19:12:39 +0100

    server configs

--- strapi/config/env/production/server.js
+++ strapi/config/env/production/server.js
@@ -1,12 +1,14 @@
 module.exports = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
-    url: env('API_URL', 'http://localhost/api'),
-    port: env.int('PORT', 1337),
-    admin: {
-      url: env('ADMIN_URL', 'http://localhost/dashboard'),
-      auth: {
-        secret: env('ADMIN_JWT_SECRET', '6692b34a8dcf0071824091a06bca9eb0'),
-      },
+  host: env("HOST", "0.0.0.0"),
+  url: env("API_URL", "http://localhost/api"),
+  port: env.int("PORT", 1337),
+  cron: {
+    enabled: true,
+  },
+  admin: {
+    url: env("ADMIN_URL", "http://localhost/dashboard"),
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "6692b34a8dcf0071824091a06bca9eb0"),
     },
-  });
-  
+  },
+});
--- strapi/config/server.js
+++ strapi/config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   // url: env("API_URL", "http://localhost:1337/api"),
   port: env.int("PORT", 1337),
   admin: {
-    // url: env("ADMIN_URL", "http://localhost:1337/dashboard"),
+    //"path": "/dashboard",
     auth: {
       secret: env("ADMIN_JWT_SECRET", "6692b34a8dcf0071824091a06bca9eb0"),
     },
